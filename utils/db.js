7acf5d3d566d3a9d028b4a640b38ea302b2878b1
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-03-09 15:10:00 +0100

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,34 @@
+/* MongoDB utils */
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
+    // Create a new instance of MongoClient with the provided connection URL
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    // Establish a connection to the MongoDB server
+    this.client.connect();
+  }
+
+  isAlive() {
+    // returns true when the connection to MongoDB is a success otherwise, false
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    // returns the number of documents in the collection users.
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    // returns the number of documents in the collection files.
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
