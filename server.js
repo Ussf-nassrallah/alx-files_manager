c56bbb26523e6152c2556df7f99d39c0534cad78
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-03-10 10:05:23 +0000

    Done: Configure routes for status and stats endpoints

--- server.js
+++ server.js
@@ -2,12 +2,11 @@
 import express from 'express';
 import configureRoutes from './routes';
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 configureRoutes(app);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
