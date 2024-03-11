27ab75dbc144249a872f108d4ac1cb84991794db
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-03-11 16:33:34 +0000

    fix eslint issues

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import configureRoutes from './routes';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '50mb' }));
-
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
